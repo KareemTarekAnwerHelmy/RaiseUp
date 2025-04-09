2447954d504e1ca704e8ce5fc86f34e831af1b83
Abdallah-Ali247 <aa2447454@gmail.com>
2025-04-09 17:21:20 +0200

    finish create new category

--- RaiseUp/admin_dashboard/urls.py
+++ RaiseUp/admin_dashboard/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from admin_dashboard.views import landing, create_new_category, delete_specific_category, edit_specific_category, mark_featured
+from admin_dashboard.views import *
 
 
 urlpatterns = [
     path('', landing, name='admin_dashboard'),
+    path('create_category/', create_new_category, name='create_category'),
     
 ]
--- RaiseUp/admin_dashboard/views.py
+++ RaiseUp/admin_dashboard/views.py
@@ -4,7 +4,23 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from .decorators import admin_required
 
+from projects.forms import Category_ModelForm
+
+
 @login_required(login_url='login')
 @admin_required
 def landing(request):
     return render(request, "admin_dashboard/dashboard.html")
+
+
+
+@login_required(login_url='login')
+@admin_required
+def create_new_category(request):
+    form = Category_ModelForm()
+    if request.method == 'POST':
+        form = Category_ModelForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return render(request, 'admin_dashboard/dashboard.html')
+    return render(request, 'admin_dashboard/create_category.html', context={"categoryForm": form})
