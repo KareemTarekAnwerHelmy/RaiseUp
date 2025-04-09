fb9ed01250a2da677fa2c28f0feaee9458e4a963
Abdallah-Ali247 <aa2447454@gmail.com>
2025-04-09 17:25:52 +0200

    finish edite category

--- RaiseUp/admin_dashboard/urls.py
+++ RaiseUp/admin_dashboard/urls.py
@@ -5,5 +5,6 @@ from admin_dashboard.views import *
 urlpatterns = [
     path('', landing, name='admin_dashboard'),
     path('create_category/', create_new_category, name='create_category'),
+    path('edit/<int:category_id>', edit_specific_category, name="edit_category"),
     
 ]
--- RaiseUp/admin_dashboard/views.py
+++ RaiseUp/admin_dashboard/views.py
@@ -4,7 +4,6 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from .decorators import admin_required
 
-from projects.forms import Category_ModelForm
 
 
 @login_required(login_url='login')
@@ -13,6 +12,7 @@ def landing(request):
     return render(request, "admin_dashboard/dashboard.html")
 
 
+from projects.forms import Category_ModelForm
 
 @login_required(login_url='login')
 @admin_required
@@ -24,3 +24,18 @@ def create_new_category(request):
             form.save()
             return render(request, 'admin_dashboard/dashboard.html')
     return render(request, 'admin_dashboard/create_category.html', context={"categoryForm": form})
+
+
+from projects.models import Category, Project
+
+@login_required(login_url='login')
+@admin_required
+def edit_specific_category(request, category_id):
+    selected_category = Category.get_category_by_id(category_id)
+    form = Category_ModelForm(instance=selected_category)
+    if request.method == 'POST':
+        form = Category_ModelForm(request.POST, instance=selected_category)
+        if form.is_valid():
+            form.save()
+            return redirect('all_categories')
+    return render(request, 'admin_dashboard/edit_category.html', context={"categoryForm": form})
