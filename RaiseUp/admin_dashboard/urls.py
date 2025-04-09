d5a5c92f9f08c66ed59de64519e07de184075cec
Abdallah-Ali247 <aa2447454@gmail.com>
2025-04-09 17:31:43 +0200

    finish admin dashboard

--- RaiseUp/admin_dashboard/urls.py
+++ RaiseUp/admin_dashboard/urls.py
@@ -5,6 +5,7 @@ from admin_dashboard.views import *
 urlpatterns = [
     path('', landing, name='admin_dashboard'),
     path('create_category/', create_new_category, name='create_category'),
+    path('edit_category/', create_new_category, name='create_category'),
     path('edit/<int:category_id>', edit_specific_category, name="edit_category"),
     path('delete/<int:category_id>', delete_specific_category, name="delete_category"),
     path('mark_featured/', mark_featured, name='mark_featured'),
