4f483277261990925b1461e4bc98dfbd0c6bd1fd
Abdallah-Ali247 <aa2447454@gmail.com>
2025-04-09 17:29:04 +0200

    finish delete category

--- RaiseUp/admin_dashboard/urls.py
+++ RaiseUp/admin_dashboard/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('create_category/', create_new_category, name='create_category'),
     path('edit/<int:category_id>', edit_specific_category, name="edit_category"),
     path('delete/<int:category_id>', delete_specific_category, name="delete_category"),
+    path('mark_featured/', mark_featured, name='mark_featured'),
     
 ]
--- RaiseUp/admin_dashboard/views.py
+++ RaiseUp/admin_dashboard/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect, reverse
+from django.http import HttpResponse
 
 # Create your views here.
 from django.contrib.auth.decorators import login_required
@@ -49,3 +50,39 @@ def delete_specific_category(request, category_id):
     deleted_category.delete()
     url = reverse('all_categories')
     return redirect(url)
+
+
+from django.http import JsonResponse
+from django.utils import timezone
+
+@login_required(login_url='login')
+@admin_required
+def mark_featured(request):
+    if request.method == 'POST':
+        project_id = request.POST.get('project_id')
+        is_featured = request.POST.get('is_featured')
+        try:
+            project = Project.objects.get(pk=project_id)
+            project.is_featured = is_featured == 'on'
+            if project.is_featured:
+                project.featured_at = timezone.now()  # set featured_at to current date and time
+            project.save()
+            return redirect('all_projects')
+        except Exception as e:
+            return JsonResponse({'error': str(e)}, status=500)
+    return redirect('all_projects')
+
+def page_403(request):
+    return render(request, "403.html")
+
+
+
+
+
+
+
+
+
+
+
+
